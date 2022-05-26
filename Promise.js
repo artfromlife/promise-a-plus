583fab375fc25255ed93ba6e6c74e067e77a8f38
uyu <424160958@qq.com>
2022-05-26 13:10:15 +0800

    then ?

--- Promise.js
+++ Promise.js
@@ -1,113 +1,140 @@
-
 function resolve(value) {
-  setTimeout(() => {
-    if (this.status === STATUS.PENDING) {
-      this.value = value
-      this.status = STATUS.FULFILLED
-      this.resolveCallbacks.forEach(cb => cb(value))
-    }
-  }, 0)
+    setTimeout(() => {
+        if (this.status === STATUS.PENDING) {
+            this.value = value
+            this.status = STATUS.FULFILLED
+            this.resolveCallbacks.forEach(cb => cb(value))
+        }
+    }, 0)
 }
 
 function reject(value) {
-  setTimeout(() => {
-    if (this.status === STATUS.PENDING) {
-      this.value = value
-      this.status = STATUS.REJECTED
-      this.rejectCallbacks.forEach(cb => cb(value))
-    }
-  },0)
+    setTimeout(() => {
+        if (this.status === STATUS.PENDING) {
+            this.value = value
+            this.status = STATUS.REJECTED
+            this.rejectCallbacks.forEach(cb => cb(value))
+        }
+    }, 0)
 }
+
 const STATUS = {
-  PENDING : 1 ,
-  FULFILLED: 2 ,
-  REJECTED: 3
+    PENDING: 1,
+    FULFILLED: 2,
+    REJECTED: 3
 }
+
 class Promise {
-  constructor(executor) {
-    this.status = STATUS.PENDING
-    this.value = null
-    this.resolveCallbacks = []
-    this.rejectCallbacks = []
-    try {
-      executor(resolve.bind(this), reject.bind(this))
-    }catch (e) {
-      reject.call(this,e)
+    constructor(executor) {
+        this.status = STATUS.PENDING
+        this.value = null
+        this.resolveCallbacks = []
+        this.rejectCallbacks = []
+        try {
+            executor(resolve.bind(this), reject.bind(this))
+        } catch (e) {
+            reject.call(this, e)
+        }
     }
-  }
-  static resolve(value){
-    return new Promise((resolve,reject) => {
-      resolve(value)
-    })
-  }
-  
-  static reject(value){
-    return new Promise((resolve,reject) => {
-      reject(value)
-    })
-  }
-  
-  static race(promises) {
-    return new Promise((resolve,reject) => {
-      promises.forEach(promise => promise.then((resolve,reject)))
-    })
-  }
-  
-  static any(promises) {
-    let count = 0
-    const result = []
-    return new Promise((resolve,reject) => {
-      promises.forEach(promise => promise.then((res => {
-        ++count
-        result.push(res)
-        if(count === promises.length){
-          resolve(result)
+
+    static resolve(value) {
+        return new Promise((resolve, reject) => {
+            resolve(value)
+        })
+    }
+
+    static reject(value) {
+        return new Promise((resolve, reject) => {
+            reject(value)
+        })
+    }
+
+    static race(promises) {
+        return new Promise((resolve, reject) => {
+            promises.forEach(promise => promise.then((resolve, reject)))
+        })
+    }
+
+    static all(promises) {
+        let count = 0
+        const result = []
+        return new Promise((resolve, reject) => {
+            promises.forEach(promise => promise.then((res => {
+                ++count
+                result.push(res)
+                if (count === promises.length) {
+                    resolve(result)
+                }
+            }, reject)))
+        })
+    }
+
+    then(onFulfilled, onRejected) {
+        return new Promise((resolve, reject) => {
+            try {
+                onFulfilled = typeof onFulfilled === "function" ? onFulfilled : v => v
+                onRejected = typeof onRejected === "function" ? onRejected : v => v
+                if (this.status === STATUS.FULFILLED) {
+                    const fulfilledValue = onFulfilled(this.value)
+                    if (fulfilledValue instanceof Promise) {
+                        fulfilledValue.then(resolve, reject)
+                    } else {
+                        resolve(this.value)
+                    }
+                } else if (this.status === STATUS.REJECTED) {
+                    const rejectedValue = onRejected(this.value)
+                    if(rejectedValue instanceof Promise){
+                        rejectedValue.then(resolve,reject)
+                    }else {
+                        reject(this.value)
+                    }
+                } else {
+                    this.resolveCallbacks.push(() => {
+                        const fulfilledValue = onFulfilled(this.value)
+                        if (fulfilledValue instanceof Promise) {
+                            fulfilledValue.then(resolve, reject)
+                        } else {
+                            resolve(this.value)
+                        }
+                    })
+                    this.rejectCallbacks.push(() => {
+                        const rejectedValue = onRejected(this.value)
+                        if(rejectedValue instanceof Promise){
+                            rejectedValue.then(resolve,reject)
+                        }else {
+                            reject(this.value)
+                        }
+                    })
+                }
+            } catch (e) {
+                reject(e)
+            }
+        })
+    }
+
+    catch(onRejected) {
+        return this.then(null, onRejected)
+    }
+
+    finally(cb) {
+        if (this.status === STATUS.PENDING) {
+            this.resolveCallbacks.push(cb)
+            this.rejectCallbacks.push(cb)
+        } else {
+            cb()
         }
-      },reject)))
-    })
-  }
-  
-  then (onFulfilled , onRejected) {
-    return new Promise((resolve, reject) => {
-      try {
-        onFulfilled = typeof onFulfilled === "function" ?　onFulfilled : () => {}
-        onRejected = typeof onRejected === "function" ?　onRejected : () => {}
-        if(this.status === STATUS.FULFILLED) {
-          setTimeout(() =>　{
-            onFulfilled(this.value)
-            resolve(this.value)
-          },0)
-        }else if(this.status === STATUS.REJECTED){
-          setTimeout(() =>　{
-            onRejected(this.value)
-            reject(this.value)
-          },0)
-        }else {
-          this.resolveCallbacks.push(onFulfilled)
-          this.rejectCallbacks.push(onRejected)
-        }
-      }catch (e) {
-        reject(e)
-      }
-    })
-  }
-  catch(onRejected) {
-  
-  }
-  
-  finally() {
-  
-  }
-  
-  static defer() {
-    const df = {}
-    df.promise = new Promise((resolve,reject) => {
-      df.resolve = resolve
-      df.reject = reject
-    })
-    return df
-  }
-  static deferred = Promise.defer
+    }
+
+    static defer() {
+        const df = {}
+        df.promise = new Promise((resolve, reject) => {
+            df.resolve = resolve
+            df.reject = reject
+        })
+        return df
+    }
+
+    static deferred = Promise.defer
 }
 
 module.exports = Promise
---
+++ src/TestPromise.js
@@ -0,0 +1,3 @@
+const Promise = require('../Promise')
+
+new Promise(() => {}).then()
