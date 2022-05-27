66326fa52a7e080ab4003b346f0245bc1b37ecc1
Drinker <424160958@qq.com>
2022-05-27 23:59:28 +0800

    promise

--- src/TestPromise.js
+++ src/TestPromise.js
@@ -1,3 +1,13 @@
 const Promise = require('../Promise')
 
-new Promise(() => {}).then()
+const p = Promise.reject(1)
+p.then(null,() => {
+  throw new Error()
+}).catch(e => {})
+p.then(null,() => console.log(3))
+p.then(null,() => console.log(4))
+
+const q = Promise.resolve(1)
+q.then(()=> console.log(2))
+q.then(()=> console.log(3))
+q.then(()=> console.log(4))
